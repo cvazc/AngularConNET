177e42697331e1235e6bd9602afcd92050e8cc54
Sebastian <contreras.sebastian.dev@gmail.com>
2025-04-16 20:45:28 -0600

    chore: added Angular Material with default theme

--- angular-peliculas/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ angular-peliculas/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,10 +1,12 @@
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-listado-peliculas',
-  imports: [DatePipe, CurrencyPipe, ListadoGenericoComponent],
+  imports: [DatePipe, CurrencyPipe, ListadoGenericoComponent, MatButtonModule, MatIconModule],
   templateUrl: './listado-peliculas.component.html',
   styleUrl: './listado-peliculas.component.css',
 })
